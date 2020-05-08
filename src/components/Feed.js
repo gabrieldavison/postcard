9665e04a8f55a1027cc65b9567baab30cd85ce94
Gabriel <gabriel@DESKTOP-9M2QL5A.home>
2020-05-08 17:12:43 +0100

    Style Feed and postcard

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,6 +3,7 @@ import Timeline from "./Timeline";
 import Search from "./Search";
 import Nav from "./Nav";
 import { Link } from "@reach/router";
+import "./styles/Feed.css";
 const Feed = (props) => {
   const [activityDiscover, updateActivityDiscover] = useState("activity");
 
@@ -15,62 +16,61 @@ const Feed = (props) => {
     return props.allPostcards.filter((val) => renderIDs.includes(val.id));
   };
   return (
-    <div>
+    <div className="feed-component">
       <Nav currentUsername={props.currentUsername} logout={props.logout} />
       <Search
         handleSearch={props.handleSearch}
         updateSearchedUser={props.updateSearchedUser}
       />
-
-      <h1>Feed</h1>
-      {/* Radio buttons to switch between discover and activity */}
-      <div>
-        <label>
-          <input
-            id="activity"
-            type="radio"
-            name="activityDiscover"
-            value="activity"
-            checked={activityDiscover === "activity"}
-            onChange={(e) => updateActivityDiscover(e.target.value)}
-          />
-          Activity
-        </label>
-        <label>
-          <input
-            id="discover"
-            type="radio"
-            name="activityDiscover"
-            value="discover"
-            checked={activityDiscover === "discover"}
-            onChange={(e) => updateActivityDiscover(e.target.value)}
-          />
-          Discover
-        </label>
-      </div>
-      {activityDiscover === "activity" ? (
-        //Timeline for activity view
+      <div className="feed-container">
+        <div className="feed-header">
+          <h1>Feed</h1>
+          {/* Radio buttons to switch between discover and activity */}
+          <div className="timeline-selector">
+            <input
+              id="activity"
+              type="radio"
+              name="activityDiscover"
+              value="activity"
+              checked={activityDiscover === "activity"}
+              onChange={(e) => updateActivityDiscover(e.target.value)}
+            />
+            <label htmlFor="activity">Activity</label>
+            <input
+              id="discover"
+              type="radio"
+              name="activityDiscover"
+              value="discover"
+              checked={activityDiscover === "discover"}
+              onChange={(e) => updateActivityDiscover(e.target.value)}
+            />
+            <label htmlFor="discover">Discover</label>
+          </div>
+        </div>
+        {activityDiscover === "activity" ? (
+          //Timeline for activity view
+          props.followed.length === 0 ? (
+            <h2>You havent followed anyone yet...</h2>
+          ) : (
+            <Timeline
+              renderPostcards={props.allPostcards.filter((val) => {
+                return props.user.followed.includes(val.owner);
+              })}
+              handleLike={props.handleLike}
+              liked={props.liked}
+            />
+          )
+        ) : //Timeline for discover view
         props.followed.length === 0 ? (
           <h2>You havent followed anyone yet...</h2>
         ) : (
           <Timeline
-            renderPostcards={props.allPostcards.filter((val) => {
-              return props.user.followed.includes(val.owner);
-            })}
+            renderPostcards={discoverPostcards()}
             handleLike={props.handleLike}
             liked={props.liked}
           />
-        )
-      ) : //Timeline for discover view
-      props.followed.length === 0 ? (
-        <h2>You havent followed anyone yet...</h2>
-      ) : (
-        <Timeline
-          renderPostcards={discoverPostcards()}
-          handleLike={props.handleLike}
-          liked={props.liked}
-        />
-      )}
+        )}
+      </div>
     </div>
   );
 };
--- src/components/PostcardComponent.js
+++ src/components/PostcardComponent.js
@@ -1,31 +1,46 @@
 import React, { useState } from "react";
-import "./PostcardComponent.css";
+import "./styles/PostcardComponent.css";
 const Postcard = (props) => {
   const [textClass, updateTextClass] = useState("hidden");
-
+  const [imgClass, updateImgClass] = useState("opaque");
   return (
-    <div>
-      <img
-        src={`./userImg/${props.postcard.location}.jpg`}
-        width="700px"
-        onClick={() => {
-          //toggle hidden text
-          textClass === "hidden"
-            ? updateTextClass("visible")
-            : updateTextClass("hidden");
-        }}
-      ></img>
-      <p className={textClass}>{props.postcard.text}</p>
-      <h3>{props.postcard.date.toLocaleString()}</h3>
-      <h2>{props.postcard.owner}</h2>
-      <button onClick={() => props.handleLike(props.postcard.id)}>
-        {props.liked.includes(props.postcard.id) ? "Unlike" : "Like"}
-      </button>
-      {props.loggedIn === true ? (
-        <button onClick={() => props.handleDeletePostcard(props.postcard.id)}>
-          Delete
-        </button>
-      ) : null}
+    <div className="postcard-component">
+      <div className="image-text-container">
+        <img
+          className={`${imgClass} postcard-image`}
+          src={`./userImg/${props.postcard.location}.jpg`}
+          width="700px"
+          onClick={() => {
+            //toggle hidden text
+            imgClass === "opaque"
+              ? updateImgClass("transparent")
+              : updateImgClass("opaque");
+            textClass === "hidden"
+              ? updateTextClass("visible")
+              : updateTextClass("hidden");
+          }}
+        ></img>
+        <p className={`${textClass} postcard-text`}>{props.postcard.text}</p>
+      </div>
+      <div className="postcard-details">
+        <div className="author-date">
+          <h3>{props.postcard.date.toLocaleString()}</h3>
+          <h2>{props.postcard.owner}</h2>
+        </div>
+
+        <div className="postcard-buttons">
+          <button onClick={() => props.handleLike(props.postcard.id)}>
+            {props.liked.includes(props.postcard.id) ? "Unlike" : "Like"}
+          </button>
+          {props.loggedIn === true ? (
+            <button
+              onClick={() => props.handleDeletePostcard(props.postcard.id)}
+            >
+              Delete
+            </button>
+          ) : null}
+        </div>
+      </div>
     </div>
   );
 };
